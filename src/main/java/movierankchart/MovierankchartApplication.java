8164cb66756ad50c51711b89aa103619fbfb7568
kjb95 <topmedia44@gmail.com>
2023-07-01 15:25:15 +0900

    chore: 스프링 부트 프로젝트 초기화

---
+++ src/main/java/movierankchart/MovierankchartApplication.java
@@ -0,0 +1,13 @@
+package movierankchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierankchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierankchartApplication.class, args);
+	}
+
+}
