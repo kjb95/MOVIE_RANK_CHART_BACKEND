513565121e04915a1a6c3621261dc1d4aa999cf8
kjb95 <topmedia44@gmail.com>
2023-08-19 17:02:58 +0900

    fix: GET /v1/movie-rank/pie-chart
    - 파이 차트에 필요한 데이터 조회시, 영화 아이디도 조회

--- src/main/java/movierankchart/domain/movierank/dto/response/FindMovieRankPieChartResponseDto.java
+++ src/main/java/movierankchart/domain/movierank/dto/response/FindMovieRankPieChartResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class FindMovieRankPieChartResponseDto {
-    private String movieTitle;
+    private Long id;
+    private String name;
     private Long value;
 }
